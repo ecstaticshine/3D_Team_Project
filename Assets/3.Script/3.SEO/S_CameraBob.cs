00335a5f70bcbede2b385e02f7ae09efd78c3e9b
J5-67 <gjong567@gmail.com>
2025-12-16 11:28:23 +0900

    [S] Bullet Line

--- Assets/3.Script/3.SEO/S_CameraBob.cs
+++ Assets/3.Script/3.SEO/S_CameraBob.cs
@@ -17,20 +17,22 @@ public class S_CameraBob : MonoBehaviour
     void Update()
     {
         float speed = player.GetComponent<CharacterController>().velocity.magnitude;
-
-        if (speed > 0.1f) // ∞»∞Ì ¿÷¥Ÿ∏È
+        if (!player.isJumping)
         {
-            timer += Time.deltaTime * bobSpeed;
+            if (speed > 0.1f) // ∞»∞Ì ¿÷¥Ÿ∏È
+            {
+                timer += Time.deltaTime * bobSpeed;
 
-            float newY = defaultPosY + Mathf.Sin(timer) * bobAmount;
+                float newY = defaultPosY + Mathf.Sin(timer) * bobAmount;
 
-            transform.localPosition = new Vector3(transform.localPosition.x, newY, transform.localPosition.z);
-        }
-        else
-        {
-            timer = 0;
-            float newY = Mathf.Lerp(transform.localPosition.y, defaultPosY, Time.deltaTime * bobSpeed);
-            transform.localPosition = new Vector3(transform.localPosition.x, newY, transform.localPosition.z);
+                transform.localPosition = new Vector3(transform.localPosition.x, newY, transform.localPosition.z);
+            }
+            else
+            {
+                timer = 0;
+                float newY = Mathf.Lerp(transform.localPosition.y, defaultPosY, Time.deltaTime * bobSpeed);
+                transform.localPosition = new Vector3(transform.localPosition.x, newY, transform.localPosition.z);
+            }
         }
     }
 }
