a284f331baf00d02d3c2b322acefbbf6e6e43be8
J5-67 <gjong567@gmail.com>
2025-12-26 11:44:52 +0900

    Tutorial -> Stage 1
    튜토리얼 -> 스테이지 1 바로 가는 발판 추가

---
+++ Assets/3.Script/0.MainGame/99.ETC/SceneLoaderPad.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderPad : MonoBehaviour
+{
+    [Header("이동할 씬 이름")]
+    [SerializeField] private string targetSceneName = "Stage1";
+
+    [Header("저장 여부")]
+    [SerializeField] private bool shouldSave = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MoveToScene();
+        }
+    }
+
+    private void MoveToScene()
+    {
+        if (shouldSave && SaveManager.instance != null)
+        {
+            SaveManager.instance.saveData.sceneToLoad = targetSceneName;
+
+            SaveManager.instance.SaveGame();
+
+            Debug.Log($"[유니] {targetSceneName} 진입 전 저장 완료!");
+        }
+
+        SceneManager.LoadScene(targetSceneName);
+    }
+}
